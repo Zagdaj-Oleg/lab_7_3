bf44df9a9e590cdea68327cc75fd9b6dab3f5b66
Zagdaj-Oleg <72668217+Zagdaj-Oleg@users.noreply.github.com>
2020-11-21 18:27:21 +0200

    lab_7_3

---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_6_1/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = Sum(2, 3);
+			Assert::AreEqual(t, 5);
+		}
+	};
+}
